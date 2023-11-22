69a49fc8d50a4da2bde3c74d99cbd03e9f12d41b
Olya Tatianina <olya.tatianina@gmail.com>
2023-11-22 19:48:03 +0400

    web.yml

--- e2e/start.test.js
+++ e2e/start.test.js
@@ -6,8 +6,8 @@ describe('Page start', () => {
 
   beforeEach(async () => {
     browser = await puppeteer.launch({
-      headless: false,
-      slowMo: 100,
+      headless: process.env.CI,
+      slowMo: 250,
       devtools: true,
     });
 
